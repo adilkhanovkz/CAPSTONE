00b57662420e7973263dda5973c2dc15095aab5a
Aidos Adilkhanov <adilkhanovkz@gmail.com>
2024-07-03 23:23:37 +0500

    init

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </React.Fragment>
   );
 }
 
